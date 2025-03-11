58354ce2e7414a887572678d5ee694527bc95fcc
jamalihassan0307 <jamalihassan0307@gmail.com>
2025-03-11 19:19:31 +0500

    favpraites page

--- lib/pages/favorites_page.dart
+++ lib/pages/favorites_page.dart
@@ -6,38 +6,113 @@ import 'package:uidesign08/widgets/hotel_card.dart';
 class FavoritesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Consumer<FavoritesProvider>(
-      builder: (context, favoritesProvider, child) {
-        final favorites = favoritesProvider.favorites;
-        
-        if (favorites.isEmpty) {
-          return Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(Icons.favorite_border, size: 64, color: Colors.grey),
-                SizedBox(height: 16),
-                Text(
-                  'No favorites yet',
-                  style: TextStyle(fontSize: 18, color: Colors.grey),
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      body: SafeArea(
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              floating: true,
+              backgroundColor: Colors.white,
+              elevation: 0,
+              expandedHeight: 120,
+              flexibleSpace: FlexibleSpaceBar(
+                background: Container(
+                  color: Colors.white,
+                  padding: EdgeInsets.all(20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        'Favorites',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        'Your dream destinations',
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ],
+              ),
             ),
-          );
-        }
+            Consumer<FavoritesProvider>(
+              builder: (context, favoritesProvider, child) {
+                final favorites = favoritesProvider.favorites;
 
-        return GridView.builder(
-          padding: EdgeInsets.all(20),
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            childAspectRatio: 0.7,
-            crossAxisSpacing: 20,
-            mainAxisSpacing: 20,
-          ),
-          itemCount: favorites.length,
-          itemBuilder: (context, index) => HotelCard(item: favorites[index]),
-        );
-      },
+                if (favorites.isEmpty) {
+                  return SliverFillRemaining(
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.favorite_border,
+                            size: 80,
+                            color: Colors.grey.shade300,
+                          ),
+                          SizedBox(height: 16),
+                          Text(
+                            'No favorites yet',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey.shade400,
+                            ),
+                          ),
+                          SizedBox(height: 8),
+                          Text(
+                            'Start adding your favorite places',
+                            style: TextStyle(
+                              color: Colors.grey.shade400,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                }
+
+                return SliverPadding(
+                  padding: EdgeInsets.all(20),
+                  sliver: SliverGrid(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      childAspectRatio: 0.7,
+                      crossAxisSpacing: 20,
+                      mainAxisSpacing: 20,
+                    ),
+                    delegate: SliverChildBuilderDelegate(
+                      (context, index) => HotelCard(
+                        item: favorites[index],
+                        onTap: () => _showBookingBottomSheet(context, favorites[index]),
+                      ),
+                      childCount: favorites.length,
+                    ),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _showBookingBottomSheet(BuildContext context, Item hotel) {
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      backgroundColor: Colors.transparent,
+      builder: (context) => BookingBottomSheet(hotel: hotel),
     );
   }
 } 
